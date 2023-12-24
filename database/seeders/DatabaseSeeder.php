a07a06839cfb9e3eb85217814e4834c55b6e2f51
Ye Naing Tun <yenaingtun425@gmail.com>
2023-12-24 09:58:05 +0630

    updated database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,9 +15,26 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'SuperAdmin',
+            'role' => 'super_admin',
+            'email' => 'superadmin@mail.com',
+            'password' => bcrypt('password'),
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+        \App\Models\User::factory()->create([
+            'name' => 'HotelManager',
+            'role' => 'hotel_manager',
+            'email' => 'hotelmanager@mail.com',
+            'password' => bcrypt('password'),
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Reservation',
+            'role' => 'reservation',
+            'email' => 'reservation@mail.com',
+            'password' => bcrypt('password'),
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
     }
 }
